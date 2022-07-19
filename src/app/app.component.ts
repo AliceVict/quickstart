d11eaf34129ec7aac860476e609b4173aca6c749
AliceVict <100197631+AliceVict@users.noreply.github.com>
2022-07-19 21:03:06 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,42 @@
 import { Component } from '@angular/core';
 
+const todos = [
+  {
+    title: "Задача 1",
+    completed: false
+  },
+  {
+    title: "Задача 2",
+    completed: false
+  },
+  {
+    title: "Задача 3",
+    completed: false
+  },
+]
+
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'Angular 2'; 
+todos = todos;
+
+toggle(todo: any) {
+
+  todo.completed = !todo.completed;
+  // console.log("Переключение:",todo);
+}
+
+delete(todo: any) {
+  let index = this.todos.indexOf(todo);
+
+  if (index > -1) this.todos.splice(index,1);
+
+}
+
+
+}
